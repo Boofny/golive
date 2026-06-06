754632d5aa4b14b0b67c53e7ee6dbf264c2910cd
Boofny <Brandonpizano7557@gmail.com>
2026-06-06 10:30:14 -0400

    starting some tests

--- golive_test.go
+++ golive_test.go
@@ -3,3 +3,26 @@ package golive
 import (
 	"testing"
 )
+
+type User struct{
+	ID 		int 		`json:"id"`
+	Name 	string 	`json:"name"`
+}
+
+const (
+	userJson = `{"id": 1, "name": "David"}`
+	usersJson = `[{"id": 1, "name": "David"}]`
+)
+
+const (
+	PretyJson = `
+	{
+		"id": 1,
+		"name": "David"
+	}
+	`
+)
+
+func TestGolive(t *testing.T){
+}
+
